e733dab83785a0dbf6eafd1c946b2a0e8a482255
Ryan Erickson <rerickson21@yahoo.com>
2020-06-05 19:48:51 -0400

    Phase 1

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Loading from './components/Loading/Loading';
+import Speech from './components/Speech/Speech';
+import Sorting from './components/Sorting/Sorting';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: false,
+      speechIndex: 0,
+      sortIndex: 0,
+      sorting: false,
+    };
+  }
+  handleSpeechChange = (bool) => {
+    this.setState((prevState) => {
+      if (bool){
+        return {
+          speechIndex: prevState.speechIndex + 1,
+        }
+      } else {
+        return {
+          speechIndex: prevState.speechIndex === 0 ? 0 : prevState.speechIndex - 1,
+        }
+      }
+    }, () => this.state.speechIndex > 9 ? this.setState({sorting: true}) : null);
+  }
+  handleSkipSpeech = () => {
+    this.setState({ sorting: true });
+  }
+  render() {
+    return (
+      this.state.loading ? <Loading /> :
+      this.state.sorting ? <Sorting /> :
+      <div>
